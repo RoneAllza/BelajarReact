2e2e4c4b444c8535f2864ba78e198e767f4cf74a
RoneAllza <qiqiabdulaziz@gmail.com>
2025-12-05 08:26:52 +0700

    Video 6: React Router

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import FormLogin from "../components/Fragments/FormLogin"
 import AuthLayout from "../components/Layouts/AuthLayout"
 
@@ -5,6 +6,9 @@ const LoginPage = () => {
     return (
        <AuthLayout title="Sign In">
             <FormLogin />
+            <p className="text-sm mt-5">Don't have an account? { " " }
+                <Link to="/register" className="text-center font-bold text-blue-600">Sign Up</Link>
+                </p>
        </AuthLayout>
     )
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import FormRegister from "../components/Fragments/FormRegister"
 import AuthLayout from "../components/Layouts/AuthLayout"
 
@@ -5,6 +6,9 @@ const RegisterPage = () => {
     return (
        <AuthLayout title="Register">
             <FormRegister />
+            <p className="mt-5 text-sm">Already have an account? { " " } 
+                <Link to="/login" className="text-center font-bold text-blue-600">Sign In</Link>
+                </p>
        </AuthLayout>
     )
 }
