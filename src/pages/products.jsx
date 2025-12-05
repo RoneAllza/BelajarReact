b6cfca03291bdf351f9ddf28923c96ffc28ca786
RoneAllza <qiqiabdulaziz@gmail.com>
2025-12-05 10:03:31 +0700

    Video 9: List Rendering (PENTING! Buat integrasi dengan API)

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,23 +1,42 @@
-import Button from "../components/Elements/Button/index.jsx"
 import CardProduct from "../components/Fragments/CardProduct.jsx"
+
+const products = [
+    {
+        id: 1,
+        title: "Sepatu Baru",
+        alt: "Shoes",
+        price: "Rp.2.000.000",
+        image: "/images/shoes-1.jpg",
+        description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                   Ipsum dignissimos vero vitae consequuntur quae. 
+                   Neque aliquid similique odit veritatis laborum sit error soluta doloremque nobis, 
+                   dignissimos est. In, corporis consectetur.`,
+        link: "/products/1"
+    },
+    {
+        id: 2,
+        title: "Sepatu Lama",
+        alt: "Shoes",
+        price: "Rp.200.000",
+        image: "/images/shoes-1.jpg",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        link: "/products/2"
+    }
+]
 
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" alt="Shoes" link="/products/1" />
-                <CardProduct.Body title="Sepatu Baru" link="/products/1">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum dignissimos vero vitae consequuntur quae. Neque aliquid similique odit veritatis laborum sit error soluta doloremque nobis, dignissimos est. In, corporis consectetur.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp.2.000.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" alt="Shoes" link="/products/2" />
-                <CardProduct.Body title="Sepatu Baru" link="/products/2">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum dignissimos vero vitae consequuntur quae. Neque aliquid similique odit veritatis laborum sit error soluta doloremque nobis, dignissimos est. In, corporis consectetur.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp.2.000.000" />
-            </CardProduct>
+            
+            {products.map((product)=>(
+                <CardProduct>
+                    <CardProduct.Header image={product.image} alt={product.alt} link={product.link} />
+                    <CardProduct.Body title={product.title} link={product.link}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     )
 }
